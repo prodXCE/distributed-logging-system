609322044ddcbf997650d59f244467ce04c320a8
prodXCE <bhattacharjeeakash71@gmail.com>
2025-06-30 18:58:48 +0530

    Initial commit

---
+++ src/main/java/com/service/logaggregator/LogAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.service.logaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAggregatorApplication.class, args);
+	}
+
+}
